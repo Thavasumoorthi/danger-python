3c14bb268aee6d648d7fb06357c3bc8c934de4ef
Thavasumoorthi <harshini.b@tringapps.com>
2025-02-04 10:42:10 +0530

    modified danger file

--- dangerfile.py
+++ dangerfile.py
@@ -1,26 +1,3 @@
 from danger_python import danger, markdown
 
-
-
-# Get the PR titles
-title = danger.github.pr.title
-markdown(f"PR Title: {title}")
-
-# Add this line outside of any function or loop
-markdown("✅ Danger-python working")
-
-# Check if any files contain 'TODO'
-todo_files = []
-
-# Loop through changed files and check for 'TODO'
-for file in danger.github.pr.changed_files:
-    if 'TODO' in file.content:
-        todo_files.append(file.filename)
-
-# Display a warning if any TODOs are found
-if todo_files:
-    markdown("⚠️ Files with 'TODO' found:")
-    for file in todo_files:
-        markdown(f"- {file}")
-else:
-    markdown("✅ No TODOs found in the changed files.")
+markdown("danger working fine ")
